6ed1d5493ad7186a76c382ed0feb9aa82b0bbbe5
Ralph Gootee III <ralph@macboo.gateway.2wire.net>
2009-12-07 01:08:31 -0800

    Structured code, added homescreen, random words

--- src/com/solidsushi/wordnik/WordnikActivity.java
+++ src/com/solidsushi/wordnik/WordnikActivity.java
@@ -1,40 +1,36 @@
 package com.solidsushi.wordnik;
 
-import java.io.BufferedInputStream;
-import java.io.InputStream;
-import java.net.URL;
-import java.net.URLConnection;
-
-import org.apache.http.util.ByteArrayBuffer;
-import org.json.JSONArray;
-import org.json.JSONObject;
-
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.text.SpannableString;
-import android.text.SpannableStringBuilder;
-import android.util.Log;
+import android.text.Html;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.ScrollView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class WordnikActivity extends Activity implements OnClickListener{
     
-	static final String API_KEY = "7741b711eec09c12e05070046d60da6b92e03750359859fde";
 	private static final String TAG = WordnikActivity.class.getSimpleName();
+	
+	private static final int AOK=0,ERR=1,RANDOM=2;
 	
 	private ProgressDialog progressDialog = null;
 	private TextView mDefinitionView;
 	private EditText mWordEdit;
-	private Handler mHandler;
+	private WordHandler mHandler;
+
+	private TextView mExampleView;
+	private ScrollView mScroller;
+	private View mMainScreen;
+	private View mFeedScreen;	
 	
 	/** Called when the activity is first created. */
     @Override
@@ -47,107 +43,163 @@ public class WordnikActivity extends Activity implements OnClickListener{
         setTheme(android.R.style.Theme_Light_NoTitleBar);
         setContentView(R.layout.main);
          
-    	mDefinitionView = (TextView)findViewById(R.id.defView);
-		mDefinitionView.setFocusable(true);
-		
 		mWordEdit = (EditText)(findViewById(R.id.wordEdit));
-        View v = findViewById(R.id.goButton);
+		
+		View v = findViewById(R.id.goButton);
         v.setOnClickListener(this);
         
-        mHandler = new Handler(){
-        	@Override
-			public void handleMessage(Message msg)
-        	{
-        		mDefinitionView.setText(msg.getData().getString("def"));
-        	}
-        };
+        mScroller = (ScrollView)findViewById(R.id.scroller);
+        
+        // Inflate the main screen view
+        LayoutInflater inflater = (LayoutInflater)getSystemService(LAYOUT_INFLATER_SERVICE);	
+        mMainScreen = inflater.inflate(R.layout.home, mScroller);
+        mFeedScreen = inflater.inflate(R.layout.feed, null);
+        
+        View random = mMainScreen.findViewById(R.id.random);
+        random.setOnClickListener(this);
+        
+        mDefinitionView = (TextView)mFeedScreen.findViewById(R.id.defView);
+		mExampleView = (TextView)mFeedScreen.findViewById(R.id.exampleView);
+		
+        mHandler = new WordHandler(); 
     }
 
 	public void onClick(View v) {
 		
+		String word;
+		Thread thread;
+		
 		switch(v.getId()){
+		
 		case R.id.goButton:
 			
-			progressDialog = ProgressDialog.show(this, "Working..", 
-					"Looking up " + mWordEdit.getText().toString() + "...", true,
+			word = mWordEdit.getText().toString();
+			
+			progressDialog = ProgressDialog.show(this, "Working", 
+					"Looking up " + word + "...", true,
 	                false);
 			
-			Thread thread = new Thread(new Loader());
+			thread = new Thread(new Loader(word));
 		    thread.start();
 			
+			break;
+			
+		case R.id.random:
+			
+			progressDialog = ProgressDialog.show(this, "Working", 
+					"Finding a random word...", true,
+	                false);
+			
+			thread = new Thread(new RandomLoader());
+		    thread.start();
 			break;
 		}
 	}
 	
-	private void buildDefinition(SpannableStringBuilder def)
-	{
-		String word = mWordEdit.getText().toString();
-		String response = null;  
-		
-		try {  
-               URL updateURL = new URL("http://api.wordnik.com/api/word.json/"+word+"/definitions");  
-               URLConnection conn = updateURL.openConnection();  
-               conn.addRequestProperty("api_key", API_KEY);
-               
-               InputStream is = conn.getInputStream();  
-               
-               BufferedInputStream bis = new BufferedInputStream(is);  
-               ByteArrayBuffer baf = new ByteArrayBuffer(50);  
- 
-               int current = 0;  
-               while((current = bis.read()) != -1){  
-                   baf.append((byte)current);  
-               }  
- 
-               /* Convert the Bytes read to a String. */  
-               response = new String(baf.toByteArray());   
-		} catch (Exception e) {  
-			Toast.makeText(this, "Error:" + e, Toast.LENGTH_LONG);
-		}  
-		
-        // Turn the JSON into a structure   
-        try {       	
-        	JSONArray obj = new JSONArray(response);       	
-        	
-        	for(int i=0;i<obj.length();i++){	
-        		JSONObject o = obj.getJSONObject(i);
-        		
-        		String pos = o.optString("partOfSpeech");
-        		String summary = o.getString("defTxtSummary");
-
-        		def.append(i + ". " + pos + " " + summary + "\n\n");
- 
-        	}
-		} catch (Exception e) {
-			Log.e(TAG,"Error" + e);
-		}
-		
-	}
-
-	private void loadDefinition() {
 	
-		SpannableStringBuilder string = new SpannableStringBuilder("");
-		buildDefinition(string);
-		
-		Message msg = new Message();
-		
-		Bundle b = new Bundle();
-		b.putString("def", string.toString());
-		
-		msg.setData(b);
-   		mHandler.sendMessage(msg);
-	}
-	
-	private class Loader implements Runnable{
+	private class RandomLoader implements Runnable{
 
 		public void run() {
-			InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-			imm.hideSoftInputFromWindow(mWordEdit.getWindowToken(), 0);
 			
-			loadDefinition();
-			progressDialog.dismiss();
+			Bundle b = new Bundle();
+			Message msg = new Message();
+			msg.setData(b);
+			msg.arg1 = RANDOM;
+
+			String word = WordnikHelper.buildRandomWord();
+			
+			// Send our word back to the progress dialog
+			b.putString("word",word);
+			mHandler.sendMessage(msg);
+		}
+	}
+	
+	/** 
+	 * Uses wordnikhelper to access the web content, then sends a message 
+	 * back to the main GUI
+	 */
+	private class Loader implements Runnable{
+
+		String mWord;
+		
+		public Loader(String word)
+		{
+			mWord = word;
 		}
 		
+		public void run() {
+			Message msg = new Message();
+			
+			InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+			imm.hideSoftInputFromWindow(mWordEdit.getWindowToken(), 0);
+			String word = mWord;
+				
+			boolean found = WordnikHelper.wordExists(word);
+			
+			if(found){	
+				Bundle b = new Bundle();
+				msg.setData(b);	
+				
+				String string = WordnikHelper.buildDefinition(word);
+											
+				b.putString("def", string);
+				
+				string = WordnikHelper.buildExample(word);
+				b.putString("example", string);
+				
+				msg.arg1 = AOK;		
+			}else{
+				msg.arg1 = ERR;
+				
+			}
+				
+			progressDialog.dismiss();
+			mHandler.sendMessage(msg);
+		}		
+	}
+	
+	
+	/**
+	 * Handles the results from the thread
+	 */
+	private class WordHandler extends Handler
+	{		
+    	@Override
+		public void handleMessage(Message msg)
+    	{
+    		if(mScroller.getChildAt(0) != mFeedScreen){
+    			mScroller.removeAllViews();
+    			mScroller.addView(mFeedScreen, 0);
+    		}
+    		
+    		Bundle b = msg.getData();
+    			
+    		switch(msg.arg1){
+    		   		
+    		case AOK:  			
+    			mDefinitionView.setText(Html.fromHtml(
+    					b.getString("def")),
+    					TextView.BufferType.SPANNABLE);
+    			
+    			mExampleView.setText(Html.fromHtml(
+    					b.getString("example")),
+    					TextView.BufferType.SPANNABLE);
+    			break;
+    		
+    		case ERR:
+    			mDefinitionView.setText("Word not found!");
+    			break;
+    			
+    		case RANDOM:
+    			
+    			mWordEdit.setText(b.getString("word"));
+    			View v = findViewById(R.id.goButton);
+    			progressDialog.dismiss();
+    			v.performClick();
+    			break;
+    		}
+    		
+    	}
 	}
 	
 }
---
+++ src/com/solidsushi/wordnik/WordnikHelper.java
@@ -0,0 +1,211 @@
+package com.solidsushi.wordnik;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+import org.apache.http.util.ByteArrayBuffer;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import android.util.Log;
+
+class WordnikHelper{
+	
+	private static final String TAG = WordnikHelper.class.getSimpleName();
+	private static final String API_KEY = "7741b711eec09c12e05070046d60da6b92e03750359859fde";
+	
+	
+	public static boolean wordExists(final String word)
+	{
+		boolean found = false;
+		String response = null;
+		
+		try {  
+               URL updateURL = new URL("http://api.wordnik.com/api/word.json/" + word);  
+               URLConnection conn = updateURL.openConnection();  
+               conn.addRequestProperty("api_key", API_KEY);
+               
+               InputStream is = conn.getInputStream();  
+               
+               BufferedInputStream bis = new BufferedInputStream(is);  
+               ByteArrayBuffer baf = new ByteArrayBuffer(50);  
+ 
+               int current = 0;  
+               while((current = bis.read()) != -1){  
+                   baf.append((byte)current);  
+               }  
+ 
+               /* Convert the Bytes read to a String. */  
+               response = new String(baf.toByteArray());   
+		
+               return true;
+               
+		} catch (Exception e) {  
+		
+			Log.e(TAG,"Error: " + e);
+		}  
+		
+		return found;
+	}
+	
+	public static String buildDefinition( final String word)
+	{
+		String response = null;  
+		try {  
+               URL updateURL = new URL("http://api.wordnik.com/api/word.json/"+word+"/definitions");  
+               URLConnection conn = updateURL.openConnection();  
+               conn.addRequestProperty("api_key", API_KEY);
+               
+               InputStream is = conn.getInputStream();  
+               
+               BufferedInputStream bis = new BufferedInputStream(is);  
+               ByteArrayBuffer baf = new ByteArrayBuffer(50);  
+ 
+               int current = 0;  
+               while((current = bis.read()) != -1){  
+                   baf.append((byte)current);  
+               }  
+ 
+               /* Convert the Bytes read to a String. */  
+               response = new String(baf.toByteArray());   
+		} catch (Exception e) {  
+			Log.v(TAG,"response:" + response );	
+			Log.e(TAG,"Error: " + e);
+		}  
+
+		String builder = "";
+		
+		
+        // Turn the JSON into a structure   
+        try {       	
+        	JSONArray obj = new JSONArray(response);       	
+        	
+        	for(int i=0;i<obj.length();i++){	
+        		JSONObject o = obj.getJSONObject(i);
+        		
+        		String pos = o.optString("partOfSpeech");
+        		builder += "<p>" + i + ". <b>" + pos + "</b> " + o.getString("defTxtSummary") + "</p>";
+        		
+        		//builder.append(summary);
+        		//builder.setSpan(new UnderlineSpan(), 0,10, 0);
+        		
+        	}
+		} catch (Exception e) {
+
+			Log.e(TAG,"Error" + e);
+		}
+		
+		return builder;
+	}
+	
+	public static String buildExample( final String word)
+	{
+		String response = null;  
+		try {  
+               URL updateURL = new URL("http://api.wordnik.com/api/word.json/"+word+"/examples");  
+               URLConnection conn = updateURL.openConnection();  
+               conn.addRequestProperty("api_key", API_KEY);
+               
+               InputStream is = conn.getInputStream();  
+               
+               BufferedInputStream bis = new BufferedInputStream(is);  
+               ByteArrayBuffer baf = new ByteArrayBuffer(50);  
+ 
+               int current = 0;  
+               while((current = bis.read()) != -1){  
+                   baf.append((byte)current);  
+               }  
+ 
+               /* Convert the Bytes read to a String. */  
+               response = new String(baf.toByteArray());   
+		} catch (Exception e) {  
+			Log.v(TAG,"response:" + response );	
+			Log.e(TAG,"Error: " + e);
+		}  
+
+		String builder = "";
+		
+        // Turn the JSON into a structure   
+        try {       	
+        	JSONArray obj = new JSONArray(response);       	
+        	
+        	for(int i=0;i<obj.length();i++){	
+        		JSONObject o = obj.getJSONObject(i);
+        		
+        		String display = o.optString("display");
+        		String title = o.optString("title");
+        		
+        		// Find the instances of our word, and then bold it
+        		display = boldWord(display,word);
+        		
+        		builder += "<p>" + i + ". " + display + " - <i>" + title + "</i></p>";
+        	
+        	}
+		} catch (Exception e) {
+
+			Log.e(TAG,"Error" + e);
+		}
+		
+		return builder;
+	}
+
+	public static String buildRandomWord()
+	{
+		String response = null;  
+		try {  
+               URL updateURL = new URL("http://api.wordnik.com/api/words.json/randomWord");          
+               response = getResponse(updateURL);
+               
+		} catch (Exception e) {  
+			Log.v(TAG,"response:" + response );	
+			Log.e(TAG,"Error: " + e);
+		}  
+
+		String word = "";
+		
+        // Turn the JSON into a structure   
+        try {       	
+        	JSONObject obj = new JSONObject(response);       	
+        	word = obj.getString("word");	
+		} catch (Exception e) {
+			Log.e(TAG,"Error" + e);
+		}
+		
+		return word;
+	}
+	
+	private static String getResponse(URL updateURL) throws IOException  {
+	      URLConnection conn = updateURL.openConnection();  
+          conn.addRequestProperty("api_key", API_KEY);
+          
+          InputStream is = conn.getInputStream();  
+          
+          BufferedInputStream bis = new BufferedInputStream(is);  
+          ByteArrayBuffer baf = new ByteArrayBuffer(50);  
+
+          int current = 0;  
+          while((current = bis.read()) != -1){  
+              baf.append((byte)current);  
+          }  
+
+          /* Convert the Bytes read to a String. */  
+          return new String(baf.toByteArray());  
+	}
+
+	private static String boldWord(String display, final String word) 
+	{	
+		int location = display.indexOf(word, 0);
+		
+		if(location != -1)
+		{
+			display = display.substring(0, location) + "<b>" 
+						+ word + "</b>" 
+						+ display.substring(location+word.length(),display.length()) ;
+		}
+		
+		return display;
+	}
+}
